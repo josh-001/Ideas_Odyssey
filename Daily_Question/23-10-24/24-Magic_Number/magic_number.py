3af0f37e633dff692a91295ac77cdf3c94cf11d0
bhanu <joshuakakarla555@gmail.com>
2024-10-23 22:44:58 +0530

    optimize solution

--- Daily_Question/23-10-24/24-Magic_Number/magic_number.py
+++ Daily_Question/23-10-24/24-Magic_Number/magic_number.py
@@ -1,3 +1,4 @@
+            #### my logic  ##
 
 a=int(input("enter the 4-digit_number: "))
 a=str(a)
@@ -7,3 +8,20 @@ if x==y:
     print("yes-magic number")
 else:
     print("not a magic number")
+
+
+
+####       SOLUTION   ##
+
+a=int(input("enter the 4-digit_number: "))
+if 1000<=a<=9999:
+    d1=a//1000
+    d2=(a//100)%10
+    d3=(a//10)%10
+    d4=a%10
+    if (d1+d2)==(d3+d4):
+        print("magic number")
+    else:
+        print("not a magic number")
+else:
+    print("not a four digit number")
