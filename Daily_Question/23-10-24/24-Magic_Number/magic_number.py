2116772224f2a538bd57ab6ab8527a5518afcd6f
bhanu <joshuakakarla555@gmail.com>
2024-10-23 22:35:35 +0530

    my_solution

--- Daily_Question/23-10-24/24-Magic_Number/magic_number.py
+++ Daily_Question/23-10-24/24-Magic_Number/magic_number.py
@@ -1,2 +1,9 @@
 
 a=int(input("enter the 4-digit_number: "))
+a=str(a)
+x=int(str(a[0]))+int(str(a[1]))
+y=int(str(a[2]))+int(str(a[3]))
+if x==y:
+    print("yes-magic number")
+else:
+    print("not a magic number")
