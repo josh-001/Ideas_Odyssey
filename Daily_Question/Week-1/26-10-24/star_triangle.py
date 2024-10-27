362e5c9a1c524c400b82a0268cea8c309c99b893
bhanu <joshuakakarla555@gmail.com>
2024-10-27 16:53:48 +0530

    solution

--- Daily_Question/Week-1/26-10-24/star_triangle.py
+++ Daily_Question/Week-1/26-10-24/star_triangle.py
@@ -1,13 +1,24 @@
 
              #### wrong solution  ### 
-def star_traingle(l):
-   for i in range(l):
-        for k in range(i,l-1):
-            print("k",k)
-            print(" ",end=" ")
-        for j in range(2*l-1):
-            print("*")
-a=int(input("enter N: "))
-star_traingle(a)
+# def star_traingle(l):
+#    for i in range(l):
+#         for k in range(i,l-1):
+#             print("k",k)
+#             print(" ",end=" ")
+#         for j in range(2*l-1):
+#             print("*")
+# a=int(input("enter N: "))
+# star_traingle(a)
 
+
+def n_star_triangle(n):
+    for i in range(1, n + 1):
+        # Print spaces to center the stars
+        print(" " * (n - i), end="")
+        # Print stars
+        print("*" * (2 * i - 1))
+
+# Input
+N = int(input("Enter the value of N: "))
+n_star_triangle(N)
 
