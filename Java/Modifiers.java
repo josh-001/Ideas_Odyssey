9135bad4abdf8e08d2193b6da4a5699d7f95737e
bhanu <joshuakakarla555@gmail.com>
2024-10-28 16:42:36 +0530

    get_Set_in private

--- Java/Modifiers.java
+++ Java/Modifiers.java
@@ -1,17 +1,44 @@
 
- // private class
-class Data {
-    // private variable
+//  // private class
+// class Data {
+//     // private variable
+//     private String name;
+// }
+
+// public class Modifiers {
+//     public static void main(String[] main){
+
+//         // create an object of Data
+//         Data d = new Data();
+
+//         // access private variable and field from another class
+//         d.name = "Programiz";
+//     }
+// }
+
+
+
+//              use the getters and setters method     //
+
+
+class Data{
     private String name;
+    
+    // getter method
+    public String getName(){
+        return this.name;
+    }
+    // setter method
+    public void setName(String name) {
+        this.name= name;
+    }
 }
-
 public class Modifiers {
-    public static void main(String[] main){
 
-        // create an object of Data
+    public static void main(String[] main){
         Data d = new Data();
 
-        // access private variable and field from another class
-        d.name = "Programiz";
+        d.setName("its a name");
+        System.out.println(d.getName());
     }
 }
