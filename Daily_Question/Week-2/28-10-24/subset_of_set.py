0b7f5ba3353584badf9450cb071099a669159605
bhanu <joshuakakarla555@gmail.com>
2024-10-28 21:47:29 +0530

    my_solution

---
+++ Daily_Question/Week-2/28-10-24/subset_of_set.py
@@ -0,0 +1,16 @@
+
+
+######              my soultion not  donetried 10mins ####
+def call_rec(x,a):
+    for i in range()
+
+
+
+####    i should call a recursive funtion but unable to figureitout  where    #####
+def subset_set(a):
+    print([])
+    for i in range(len(a)):
+        call_rec(i,a)
+
+a=[1,2,3,4]
+subset_set(a)
