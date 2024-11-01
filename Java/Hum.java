f02ade5a789a6bd4d7bae2368c694ee95eb2c781
bhanu <joshuakakarla555@gmail.com>
2024-11-01 15:44:52 +0530

    modify

---
+++ Java/Hum.java
@@ -0,0 +1,29 @@
+public class Hum {
+    // statuic belong to class not to object
+    public static void main(String[] args) {
+        Human h1=new Human(22,"s1",10000,false);
+        System.out.println(h1.populationb);
+
+        Human h2=new Human(27,"s2",250000,true);
+        // static variable doesnt depend on objects 
+        //we canb use static variables wohtoput creating objhects 
+        System.out.println(h2.populationb);
+
+        yes_me();
+    }
+ 
+    // inside the static we cant call which is not static methoid like
+    void greeting(){
+        System.out.println("hio");
+          // non static can be called and static but static cant access a nion static funtion
+    }  
+
+    // something which is not stat ic belong to object
+    static void yes_me(){
+        //greeting();  //  erro r: non-static method greeting() cannot be referenced from a static context
+
+        Hum obj=new Hum();
+        obj.greeting();   
+        System.out.println("hey yah");
+    }
+}
