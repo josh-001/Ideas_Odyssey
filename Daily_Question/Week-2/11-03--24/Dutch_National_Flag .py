f098fe91b02d4659cea6a80538851dd88360318b
bhanu <joshuakakarla555@gmail.com>
2024-11-04 00:06:08 +0530

    solution

---
+++ Daily_Question/Week-2/11-03--24/Dutch_National_Flag .py
@@ -0,0 +1,23 @@
+def sort_arrange(a):
+    low=mid=0
+    high=len(a)-1
+    while mid<=high:
+        if a[mid]==-1:
+            temp=a[mid]
+            a[mid]=a[low]
+            a[low]=temp
+            low+=1
+            mid+=1
+        elif a[mid]==1:
+            temp=a[mid]
+            a[mid]=a[high]
+            a[high]=temp
+            high-=1
+        else:
+            mid+=1
+            continue
+    print(arr)
+
+arr = [0, -1, 1, 0, -1, 1, 0, -1, -1]
+
+sort_arrange(arr)
