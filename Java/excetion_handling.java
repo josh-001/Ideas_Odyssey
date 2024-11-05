913f9bca349594b1d6194e3705871c19841b5296
bhanu <joshuakakarla555@gmail.com>
2024-11-05 16:23:04 +0530

    error

--- Java/excetion_handling.java
+++ Java/excetion_handling.java
@@ -1,15 +1,26 @@
-public class excetion_handling {
-    public static void main(String[] args) {
-        excetion_handling obj=new excetion_handling();
-        obj.method1();
-    }
-    public void method1(){
-        method2();
-    }
-    public void method2(){
-        method3();
-    }
-    public void method3(){
-        int x=5/0;
-    }
-}
+
+            //  EXCEPTION HANDING  
+// public class excetion_handling {
+//     public static void main(String[] args) {
+//         excetion_handling obj=new excetion_handling();
+//         obj.method1();
+//     }
+//     public void method1(){
+//         method2();
+//     }
+//     public void method2(){
+//         method3();
+//     }
+//     public void method3(){
+//         int x=5/0;
+//     }
+// }
+
+
+
+// public class excetion_handling {
+
+//     public static void main(String[] args) {
+//         String[] a= new String[90000000*920000*90000];   //Comes under Error 
+//     }
+// }
