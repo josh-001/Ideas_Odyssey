201dde95ff91029aadcefae8f054731395ea3c77
bhanu <joshuakakarla555@gmail.com>
2024-11-05 21:19:07 +0530

    search_2d_matrix

---
+++ Daily_Question/Week-3/11-05-24/Search_2D_Matrix.py
@@ -0,0 +1,16 @@
+####    what i tried error logic iswrong  
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         a=matrix
+#         i=len(matrix)//2
+#         j=0
+#         print(i,j)
+#         while a[i][j] !=target:
+#             if a[i][j]<target:
+#                 j=j+1
+#             elif a[i][j]>target:
+#                 i=i-1
+#             print(a[i][j])
+        
+
