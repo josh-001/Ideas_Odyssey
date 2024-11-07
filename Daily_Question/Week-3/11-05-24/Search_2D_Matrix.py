90cb53665fe41a46407c01e2563e8644b6a1af46
bhanu <joshuakakarla555@gmail.com>
2024-11-07 08:59:04 +0530

    searchMatrix

--- Daily_Question/Week-3/11-05-24/Search_2D_Matrix.py
+++ Daily_Question/Week-3/11-05-24/Search_2D_Matrix.py
@@ -14,3 +14,28 @@
 #             print(a[i][j])
         
 
+
+###         SOLUTIONN  ##
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            # Calculate the row and column from the 1D index
+            row, col = mid // n, mid % n
+            mid_value = matrix[row][col]
+
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
